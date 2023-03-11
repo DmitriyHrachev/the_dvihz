a7c3b3e85cf2f3e530c7d27d9ba923e588cac3f2
Dima Hrachev <babun5678@gmail.com>
2023-03-11 18:30:10 +0100

    add gifs

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
-console.log(123);
+import lottie from 'lottie-web';
+
+window.addEventListener('DOMContentLoaded', () => {
+  lottie.loadAnimation({
+    container: document.getElementById('lottie'), // the dom element that will contain the animation
+    renderer: 'canvas',
+    loop: true,
+    autoplay: true,
+    path: '../files/promo-animation.json', // the path to the animation json
+  });
+});
