fd40937bfd6741f48ae6e91d470c12526f262c09
Ivan-Shalnov <98489312+Ivan-Shalnov@users.noreply.github.com>
2023-03-11 18:53:13 +0100

    merge block/form

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
 import './modules/form.js';
+import lottie from 'lottie-web';
+
+window.addEventListener('DOMContentLoaded', () => {
+  lottie.loadAnimation({
+    container: document.getElementById('lottie'), // the dom element that will contain the animation
+    renderer: 'canvas',
+    loop: true,
+    autoplay: true,
+    path: '../files/promo-animation.json', // the path to the animation json
+  });
+});
