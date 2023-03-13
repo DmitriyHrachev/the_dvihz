7e6d1d6e1cbf8b8a01f4154077f85a19eae4bcc7
Dima Hrachev <babun5678@gmail.com>
2023-03-13 01:36:54 +0100

    add hovers

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import gsap from 'gsap';
 import ScrollTrigger from './modules/ScrollTrigger.js';
+import { Fancybox } from '@fancyapps/ui/src/Fancybox/Fancybox.js';
 
 import lottie from 'lottie-web';
 
