7fcc4eae84539ab5035980b6e1d7880bc1e7b63e
Dima Hrachev <babun5678@gmail.com>
2023-03-14 15:49:28 +0100

    add fixes

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import gsap from 'gsap';
 import ScrollTrigger from './ScrollTrigger.js';
 
@@ -7,27 +6,6 @@ import '../effects/titleAnimation.js';
 gsap.registerPlugin(ScrollTrigger);
 const sectionRef = document.querySelector('[data-section-form]');
 const selector = gsap.utils.selector(sectionRef);
-
-const formRef = selector('[data-form]')[0];
-const sbmtBtn = selector('button')[0];
-
-// Fort handler
-formRef.addEventListener('submit', e => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  toggleLoading();
-  axios
-    .post('/form.php', formData)
-    .then(() => (window.location.href = '/thanks.html'))
-    .catch(() => console.log('Something went wrong'))
-    .finally(() => {
-      toggleLoading();
-    });
-});
-function toggleLoading() {
-  formRef.classList.toggle('form--loading');
-  sbmtBtn.disabled = !sbmtBtn.disabled;
-}
 
 // ANIMATION
 const tl = gsap.timeline({
